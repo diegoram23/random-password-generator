bb4938126c11e1d064643a11ec35dcdffb3cc115
diegoram23 <diego.pta.oc@gmail.com>
2022-11-18 10:44:34 -0800

    New css layout

--- main.js
+++ main.js
@@ -1,62 +1,37 @@
-// DOM elements
-const resultEl = document.getElementById("result")
-const length = document.getElementById("length")
-const uppercaseEl = document.getElementById("uppercase")
-const lowercaseEl = document.getElementById("lowercase")
-const numbersEl = document.getElementById("numbers")
-const specialCharsEl = document.getElementById("special-chars")
-const generateEl = document.getElementById("generate")
-
-
-
-
-
-
-const randomFunc = {
-    lower: randomLowerCaseChars,
-    upper: randomUpperCaseChars,
-    numbers: randomNumberChars,
-    special: randomSpecialChars
-}
-
-
-
-
-
-
-
-
 // Characters that user can choose from
-const lowerCaseChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-const upperCaseChars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-const numberChars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-const specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '?', '.']
+const randomChars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+                        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
+                        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '?', '.']
+
+// DOM elements
+const resultEl1 = document.getElementById("first-pass")
+const resultEl2 = document.getElementById("second-pass")
+const lengthEl = document.getElementById("length")
+const generateEl = document.getElementById("generate")
+let generatedPassword = "";
+
+
+
+
+//Listens for what variables user selects
+generateEl.addEventListener("click", () => {
+    const length = +lengthEl.value;
+    const hasUpper = uppercaseEl.checked;
+    const hasLower = lowercaseEl.checked;
+    const hasNumbers = numbersEl.checked;
+    const hasSpecial = specialCharsEl.checked;
+
+    resultEl.innerText = generatePassword(length, hasLower, hasNumbers, hasUpper, hasSpecial)
+});
+
+
 
 // function to generate random lower case chars.
 
-function randomLowerCaseChars() {
-    let randomLowerCase = lowerCaseChars[Math.floor(Math.random() * lowerCaseChars.length)]
-    return randomLowerCase
+function getRandomChars() {
+    let generatedPassword = randomChars[Math.floor(Math.random() * randomChars.length)]
+    return generatedPassword
 }
 
-// function to generate random upper case chars.
 
-function randomUpperCaseChars() {
-    let randomUpperCase = upperCaseChars[Math.floor(Math.random() * upperCaseChars.length)]
-    return randomUpperCase
-}
-
-// function to generate random number
-
-function randomNumberChars() {
-    let randomNumber = numberChars[Math.floor(Math.random() * numberChars.length)]
-    return randomNumber
-}
-
-// function to generate random special chars.
-
-function randomSpecialChars() {
-    let randomSpecial = specialChars[Math.floor(Math.random() * specialChars.length)]
-    return randomSpecial
-}
 
