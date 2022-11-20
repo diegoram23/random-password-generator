f0bcbdb5099093d3e323f4f30e48976448f2e77e
diegoram23 <diego.pta.oc@gmail.com>
2022-11-19 20:02:11 -0800

    Added slide bar for user to choose char length

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 // Characters that user can choose from
 const characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '?', '.']
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '?', '.', '<', '>']
 
 // DOM elements
 const toggle = document.getElementById("toggleDark")
 const body = document.querySelector("body")
 const resultEl1 = document.getElementById("first-pass")
 const resultEl2 = document.getElementById("second-pass")
-let passwordLength = 13;
-let generatedPassword = "";
+let passwordLength = document.getElementById("slider")
+
 
 
 
@@ -22,8 +22,8 @@ function getRandomChar() {
 
 // function that generates random string
 function generateRandomPassword() {
-    generatedPassword = ""
-    for (let i = 0; i < passwordLength; i++) {
+    let generatedPassword = ""
+    for (let i = 0; i < passwordLength.value; i++) {
         generatedPassword += getRandomChar();
     }
     return generatedPassword
